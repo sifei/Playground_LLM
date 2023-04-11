bc748e724aeeaafb038f52e888a417da7c1e4244
Sifei <sifei@users.noreply.github.com>
2023-04-11 12:24:30 -0400

    Update download_ggml.py

--- download_ggml.py
+++ download_ggml.py
@@ -1,4 +1,5 @@
 from huggingface_hub import snapshot_download
 
 cache_dir = ".cache/huggingface/hub" # your cache directory
-snapshot_download(repo_id='Pi3141/alpaca-native-7B-ggml', cache_dir=cache_dir)
+snapshot_download(repo_id='Pi3141/alpaca-native-7B-ggml', cache_dir=cache_dir) # it has other model like for 13B, alpaca-lora version
+

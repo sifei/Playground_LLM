c51544754aab46d794a792df24347265e7d8cd14
Sifei <sifei@users.noreply.github.com>
2023-04-11 12:16:03 -0400

    Update create_embedding.py

--- create_embedding.py
+++ create_embedding.py
@@ -1,6 +1,6 @@
 from langchain.embeddings import LlamaCppEmbeddings
 
-llama = LlamaCppEmbeddings(model_path="ggml-model-q4_0.bin")
+llama = LlamaCppEmbeddings(model_path="/path/to/model/ggml-model-q4_0.bin")
 text = "this is a test sentence."
 query_result = llama.embed_query(text)
 print(query_result)
